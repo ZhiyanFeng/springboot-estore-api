da2c2095d3927efc7329e64baedb2db36cd5842a
zhiyanfeng <zhiyan.feng@outlook.com>
2019-01-15 21:33:15 -0500

    user controller and setup database in application.properties file

---
+++ springboot-estore-api/src/main/java/com/practice/ws/SpringbootEstoreApiApplication.java
@@ -0,0 +1,14 @@
+package com.practice.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEstoreApiApplication.class, args);
+	}
+
+}
+
